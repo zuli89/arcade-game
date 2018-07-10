6e47a0371d69946bd4eafa5d30274efd19866f5b
zuli89 <zulimar.lucena@gmail.com>
2018-07-10 10:42:52 -0500

    Added movement to player

--- js/app.js
+++ js/app.js
@@ -4,17 +4,16 @@
 class Creature { 
     constructor() {
         this.sprite = 'images/';
-        this.x = 2;
-        this.y = 5;
+
     }
 
-    update(dt) {
-        this.leftBoardX =  this.x > 5;
-        this.leftBoardY =  this.y < 1;
+    update(dt) {  //determines if image has left game window
+        this.leftGameX =  this.x > 5; 
+        this.leftGameY =  this.y < 1;
     }
 
     render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x * 100, this.y * 74);
+        ctx.drawImage(Resources.get(this.sprite), this.x * 100, this.y * 70);
     }
 
 
@@ -23,38 +22,75 @@ class Creature {
 class Player extends Creature {
     constructor() {
         super();
+        this.x = 2;
+        this.y = 4.5; //set to 4.5 instead of 5 to solve a spacing issue when moving player
         this.sprite += 'char-boy.png';
     }
 
-    handleInput(){
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x * 100, this.y * 90);
+    }
+
+    handleInput(input) {
+        switch (input) {
+            // Set boundaries for player movement
+            case 'left':
+                this.x = this.x > 0 ? this.x - 1 : this.x;
+                break;
+            case 'up':
+                this.y = this.y > 0 ? this.y - 1 : this.y;
+                break;
+            case 'right':
+                this.x = this.x < 4 ? this.x + 1 : this.x;
+                break;
+            case 'down':
+                this.y = this.y < 4.5 ? this.y + 1 : this.y;
+                break;
+            default:
+                break;
+        }
+        this.moving = true; //player is moving
+
         
+        if (this.y <= 0){
+            level += 1;
+            setTimeout(() => { 
+                this.x = 2;
+                this.y = 4.5;}, 400);
+        }
     }
 
 }
+//sets game level, it's incremented after each game is completed
+let level = 1;
 
 class Enemy extends Creature {
-    constructor(x, y) {
+    constructor(x, y, speed) {
         super();
         this.sprite += 'enemy-bug.png';
         this.x = x;
         this.y = y;
-        this.speed = 1 + Math.random() * 3.; //sets speeds
+        this.speed = 1 + Math.random() * level; //sets speeds
     }
 
     update(dt) {
         super.update();
-        if (this.leftBoardX) {
+        if (this.leftGameX) {
             this.x = -1;
         } else {
             this.x += this.speed * dt; //change this to make it go at dif speeds?
         }
-    }
+            }
 }
 
+ 
+
+//creates player
 const player = new Player();
 //NEED TO FIGURE THIS OUT and make it my own 
-const allEnemies = [...Array(3)].map((_,i) => new Enemy(0, i+1));
 
+// place enemies in an array 
+const allEnemies = [...Array(3)].map((_,i) => new Enemy(0, i+1));
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
