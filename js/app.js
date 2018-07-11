06d88db0c1d1d0a099437c3efff0eafa1dcc3735
zuli89 <zulimar.lucena@gmail.com>
2018-07-11 15:04:45 -0500

    Added checkCollisions function

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 class Player {
     constructor() {
         this.x = 2;
-        this.y = 4.5; //set to 4.5 to solve a spacing issue when moving player
+        this.y = 4.51; //set to 4.5 to solve a spacing issue when moving player
         this.sprite = 'images/char-boy.png';
     }
 
@@ -30,27 +30,49 @@ class Player {
                 break;
             default:
                 break;
+            }
+
+        if (this.y <= 0) {
+                level += 1;
+                console.log(level);
+
         }
         //this.moving = true; //player is moving
     }
-        //win game when arriving at end point
+    //win game when arriving at end point and clear enemies to change enemy speed
     update() {
+        checkCollisions();
+        this.win();
+    }
+
+    lose() {
+        this.x = 2;
+        this.y = 4.51;
+    }
+
+    win() {
         if (this.y <= 0){
-            level += 1;
             setTimeout(() => { 
                 this.x = 2;
-                this.y = 4.5;}, 400);
+                this.y = 4.51;}, 400);
+            allEnemies = [];
+            createEnemies();
         }
     }
-
-    checkCollisions(squish) {
-        if (this.y === squish.y) {
-
-        }
-
-    }
-
+    
 }
+
+
+function checkCollisions() {
+        for (let i = 0; i < allEnemies.length;i++) {
+            if  ((Math.abs(player.y - allEnemies[i].y) > 0 &&
+                Math.abs(player.y - allEnemies[i].y) < 0.5 &&
+                Math.abs(player.x - allEnemies[i].x) < 0.2)) {
+                console.log('squish');
+                player.lose();
+                } 
+        }
+};
 
 class Enemy {
     constructor(x, y, speed) {
@@ -87,22 +109,24 @@ let allEnemies = [];
 
 //let allEnemies = [...Array(3)].map((_,i) => new Enemy(0, i+1,  1 + Math.random() * 2));
 
-(function createEnemies() {
+createEnemies();
+
+function createEnemies() {
     // creates an array of three bugs, one on each y axis cell
     for (let i=1; i < 4; i++) {
-        let x = 0;
+        let x = -1;
         let y =  i;
-        let speed = 1 + Math.random() * 2;
+        let speed = 1 + Math.random() * level;
         allEnemies.push(new Enemy(x, y, speed));
     }
     // adds two more bugs, randomnly placed along y axis
     for (let i=0; i < 2; i++) {
-        let x = -1;
+        let x = -2;
         let y =  Math.floor((Math.random() * 3) + 1);
-        let speed = 1 + Math.random() * 2;
+        let speed = 1 + Math.random() * level;
         allEnemies.push(new Enemy(x, y, speed));
     } 
-})();
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
