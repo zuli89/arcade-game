e9a9e1e33b0e96017d5a9e5f1efb9e8fa22ed3fb
Zulimar Lucena <zulimar.lucena@gmail.com>
2018-07-10 20:51:46 -0500

    Changed function to create enemies array to add more bugs

--- js/app.js
+++ js/app.js
@@ -1,30 +1,12 @@
 
 
-//creates a parent class for the player and enemy that includes their shared properties
-class Creature { 
+//Creating player and its properties
+
+class Player {
     constructor() {
-        this.sprite = 'images/';
-
-    }
-
-    update(dt) {  //determines if image has left game window
-        this.leftGameX =  this.x > 5; 
-        this.leftGameY =  this.y < 1;
-    }
-
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x * 100, this.y * 70);
-    }
-
-
-}
-
-class Player extends Creature {
-    constructor() {
-        super();
         this.x = 2;
-        this.y = 4.5; //set to 4.5 instead of 5 to solve a spacing issue when moving player
-        this.sprite += 'char-boy.png';
+        this.y = 4.5; //set to 4.5 to solve a spacing issue when moving player
+        this.sprite = 'images/char-boy.png';
     }
 
     render() {
@@ -49,9 +31,10 @@ class Player extends Creature {
             default:
                 break;
         }
-        this.moving = true; //player is moving
-
-        
+        //this.moving = true; //player is moving
+    }
+        //win game when arriving at end point
+    update() {
         if (this.y <= 0){
             level += 1;
             setTimeout(() => { 
@@ -60,37 +43,66 @@ class Player extends Creature {
         }
     }
 
-}
-//sets game level, it's incremented after each game is completed
-let level = 1;
+    checkCollisions(squish) {
+        if (this.y === squish.y) {
 
-class Enemy extends Creature {
+        }
+
+    }
+
+}
+
+class Enemy {
     constructor(x, y, speed) {
-        super();
-        this.sprite += 'enemy-bug.png';
+        this.sprite = 'images/enemy-bug.png';
         this.x = x;
         this.y = y;
-        this.speed = 1 + Math.random() * level; //sets speeds
+        this.speed = speed;
     }
 
     update(dt) {
-        super.update();
+        this.leftGameX =  this.x > 5; 
+        this.leftGameY =  this.y < 1;
         if (this.leftGameX) {
             this.x = -1;
         } else {
             this.x += this.speed * dt; //change this to make it go at dif speeds?
         }
-            }
+    }
+    
+        render() {
+            ctx.drawImage(Resources.get(this.sprite), this.x * 100, this.y * 70);
+        }
 }
 
- 
+
+//sets game level, it's incremented after each game is completed
+let level = 1;
 
 //creates player
 const player = new Player();
-//NEED TO FIGURE THIS OUT and make it my own 
+ 
+//enemies array
+let allEnemies = [];
 
-// place enemies in an array 
-const allEnemies = [...Array(3)].map((_,i) => new Enemy(0, i+1));
+//let allEnemies = [...Array(3)].map((_,i) => new Enemy(0, i+1,  1 + Math.random() * 2));
+
+(function createEnemies() {
+    // creates an array of three bugs, one on each y axis cell
+    for (let i=1; i < 4; i++) {
+        let x = 0;
+        let y =  i;
+        let speed = 1 + Math.random() * 2;
+        allEnemies.push(new Enemy(x, y, speed));
+    }
+    // adds two more bugs, randomnly placed along y axis
+    for (let i=0; i < 2; i++) {
+        let x = -1;
+        let y =  Math.floor((Math.random() * 3) + 1);
+        let speed = 1 + Math.random() * 2;
+        allEnemies.push(new Enemy(x, y, speed));
+    } 
+})();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
