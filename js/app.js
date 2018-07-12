6e2932fa4a4238f1a5df4558e1c94891bc6fd8f2
zuli89 <zulimar.lucena@gmail.com>
2018-07-12 16:08:28 -0500

    Added 'game over' modal and resetGame function

--- js/app.js
+++ js/app.js
@@ -1,74 +1,91 @@
-//Creating player and its properties
 
+//variables used to compute game level// 
+let level = 1; //sets game level, used below to increment speed by 0.5 after each game is completed
+let levelcount = 1; //counts number of levels
+
+//variables used for computing lives//
+let lives = $('i').length; 
+let hearts = []; 
+
+//game play variables
+let collision = false; //if a collision has not happened, set to false
+let allEnemies = []; //enemies array
+
+//Creating player and its properties
 class Player {
     constructor() {
         this.x = 2;
         this.y = 4.51; //set to 4.51 to solve a spacing issue when moving player
         this.sprite = 'images/char-cat-girl.png';
+        this.moving = true; // used later to prevent player from moving when modal appears
     }
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x * 100, this.y * 90);
     }
-    //used to move player
+
+    //Player movement 
     handleInput(input) {
-        switch (input) {
-            // Sets boundaries for player movement
-            case 'left':
-                this.x = this.x > 0 ? this.x - 1 : this.x;
-                break;
-            case 'up':
-                this.y = this.y > 0 ? this.y - 1 : this.y;
-                break;
-            case 'right':
-                this.x = this.x < 4 ? this.x + 1 : this.x;
-                break;
-            case 'down':
-                this.y = this.y < 4.5 ? this.y + 1 : this.y;
-                break;
-            default:
-                break;
+        if (this.moving) { 
+            switch (input) {
+                // Sets boundaries for player movement
+                case 'left':
+                    this.x = this.x > 0 ? this.x - 1 : this.x;
+                    break;
+                case 'up':
+                    this.y = this.y > 0 ? this.y - 1 : this.y;
+                    break;
+                case 'right':
+                    this.x = this.x < 4 ? this.x + 1 : this.x;
+                    break;
+                case 'down':
+                    this.y = this.y < 4.5 ? this.y + 1 : this.y;
+                    break;
+                default:
+                    break;
+                }
+            //counts when player gets to top row, used for increasing levels
+            if (this.y <= 0) {
+                    level += 0.5;
+                    levelcount += 1;
             }
-        //counts when player gets to top row, used for increasing levels
-        if (this.y <= 0) {
-                level += 0.5;
-                levelcount += 1;
         }
     }
 
-    //win game when arriving at end point and clear enemies to change enemy speed
     update() {
-        this.win();
+        this.win(); //win game when arriving at end point
         checkCollisions();
     }
 
-    //sets position of player when it collides
-    lose() {
-        this.x = 2;
-        this.y = 4.51;
-        lives -= 1;
-        setTimeout(() => { 
-            collision = false}, 400); //chan
-        if ($('i').length > 1){
-            $('i:eq(0)').remove();
-        } else {
-            console.log('game over');
+    win() {
+        if (this.y <= 0) { //if player reaches top
+            this.x = 2; //immediately returns the player to original posistion
+            this.y = 4.51;
+            allEnemies = []; //resets enemy array so new speeds can be created when leveling up
+            //shows modal
+            $('.modal#play-next').css("visibility", "visible"); 
+            $('.overlay').css("visibility", "visible");
+            $('#level-done').html(levelcount-1); //shows which level was completed
+            player.moving = false; //disables user to move player while modal is open
         }
     }
 
-    //sets position of player when winning
-    win() {
-        if (this.y <= 0){
-            setTimeout(() => { 
-                this.x = 2;
-                this.y = 4.51;}, 400);
-            allEnemies = []; //resets enemy so new speeds can be used when leveling up
-            $('#modal').css("visibility", "visible");
+    lose() {
+        this.x = 2; //sets player to original position when it collides
+        this.y = 4.51;
+        lives -= 1; //removes one life
+        collision = false // used to stop checking for collisions after one collision is done
+        //removes one life up to 5
+        if ($('i.fa-heart').length > 1){
+            $('i.fa-heart:eq(0)').remove();
+        } else { //game over
+            allEnemies = [];
+            $('.modal#game-over').css("visibility", "visible");
             $('.overlay').css("visibility", "visible");
-            $('#level-done').html(levelcount-1);
+            player.moving = false;
         }
     }
-    
+  
 }
 
 //creates player
@@ -98,15 +115,6 @@ class Enemy {
         }
 }
 
-//sets game level, it's incremented after each game is completed
-let level = 1;
-let levelcount = 1;
-let lives = $('i').length;
-let collision = false; //if a collision has not happened, function set to false
-
-//enemies array
-let allEnemies = [];
-
 createEnemies();
 
 function createEnemies() {
@@ -127,16 +135,14 @@ function createEnemies() {
 };
 
 
-//Game play
 //check for collisions between player and enemy
 function checkCollisions() {
     if (!collision) {
         for (let i = 0; i < allEnemies.length;i++) {
             if  ((Math.abs(player.y - allEnemies[i].y) > 0 &&
                 Math.abs(player.y - allEnemies[i].y) < 0.5 &&
-                Math.abs(player.x - allEnemies[i].x) < 0.5)) {
-                collision = true;
-                console.log('squish');
+                Math.abs(player.x - allEnemies[i].x) < 0.6)) {
+                collision = true; //
                 setTimeout(() => { 
                     player.lose();}, 400);
                 }
@@ -144,16 +150,37 @@ function checkCollisions() {
     }
 };
 
-//this function handles button actions on click
+//Restarting Game after game over//
+
+for (i = 0; i < 4; i++) { //creates string with lives icon to replace removed hearts when restarting game
+    hearts.push('<i class="fas fa-heart"></i> ');
+}
+
+function resetGame(){
+    level = 1; //resets level speed
+    levelcount = 1; //resets level number
+    $('#level').html(levelcount); //updates level in header
+    $("span.lives").append(hearts.join('')); //appends lives icon
+    createEnemies();
+    player.moving = true;
+}
+
+//handles modal button actions on click
 $(document).ready(function(){
-    $("button").click(function(){
+    $("#next-level").click(function(){
         createEnemies(); //creates new enemies array
         $('#level').html(levelcount); //updates level on header
-        $('#modal').css("visibility", "hidden"); //hides modal
+        $('.modal#play-next').css("visibility", "hidden"); //hides modal
         $('.overlay').css("visibility", "hidden"); //hides overlay
+        player.moving = true;
+    });
+
+    $("#play-again").click(function(){
+        $('.modal#game-over').css("visibility", "hidden"); //hides modal
+        $('.overlay').css("visibility", "hidden"); //hides overlay
+        resetGame();
     });
 });
-
 
 
 // This listens for key presses and sends the keys to your
